62a30cb0b678628bf93c94ee4785df85b71b5be9
t.c.nieuwenhuys <t.c.nieuwenhuys@3c75896a-3ff0-c1a0-4aaa-65231e0fef79>
2010-12-10 22:29:30 +0000

    eindelijk een manier gevonden om een image te spiegelen, zo kan er met 1 image 2 kanten op worden gelopen

--- code/TowerDefence/src/Tree.java
+++ code/TowerDefence/src/Tree.java
@@ -1,15 +1,12 @@
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.net.URL;
-
 import javax.imageio.ImageIO;
 
 public class Tree extends Field
 {
-
     private BufferedImage img = null;
 
     public Tree()
@@ -25,33 +22,51 @@ public class Tree extends Field
         super.paint(g);
 
         Dimension size = getSize();
-        //upSideDown((Graphics2D) g, size);
-        g.drawImage(img, 0, 0, size.width, size.height, 0, 0, img.getWidth(null), img.getHeight(null), null);
+        // upSideDown((Graphics2D) g, size);
+        // mirrored((Graphics2D) g, size);
+        if (img != null)
+        {
+            g.drawImage(img, 0, 0, size.width, size.height, 0, 0, img
+                    .getWidth(null), img.getHeight(null), null);
+        }
     }
 
     /**
-     *
      * Turns the graphics up size down
+     * 
      * @param g Graphics2D
      * @param size Dimension
      */
     public void upSideDown(Graphics2D g, Dimension size)
     {
         // It works the same as to convert matrices (not our Matrix objects)
-        g.rotate(Math.PI);
-        g.translate(-size.width, -size.height);
+        g.scale(1, -1);
+        g.translate(0, -size.height);
     }
 
+    /**
+     * @param g Graphics2D
+     * @param size Dimension
+     */
+    public void mirrored(Graphics2D g, Dimension size)
+    {
+        g.scale(-1, 1);
+        g.translate(-size.width, 0);
+    }
+
+    /**
+     * Loads the image for the Tree object
+     */
     public void loadImage()
     {
         // Get the full url of the image (C:\Users\...\images\boom.gif)
-        URL url = Tree.class.getResource("images/boom.gif");
+        URL url = Tree.class.getResource("images/boom.png");
         try
         {
             // Load the image at location url
             img = ImageIO.read(url);
-        } catch (Exception e)
-        {
         }
+        catch (Exception e)
+        {}
     }
 }
