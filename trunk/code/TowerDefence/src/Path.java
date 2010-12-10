e39109d29cb1470fe774e3598a3804bc02fbea4a
felix.leeuwen <felix.leeuwen@3c75896a-3ff0-c1a0-4aaa-65231e0fef79>
2010-12-10 08:02:51 +0000

    aanpassingen PathNode

--- trunk/code/TowerDefence/src/Path.java
+++ trunk/code/TowerDefence/src/Path.java
@@ -10,86 +10,80 @@ import java.util.ArrayList;
  *
  * @author Administrator
  */
-public class Path {
+public class Path
+{
 
-    private ArrayList<PathNode> path;
-    private int counter;
+        private ArrayList<PathNode> path;
+        private int counter;
 
-    public Path() {
-        path = new ArrayList<PathNode>();
-        counter = 0;
-    }
-
-    public boolean hasNext() {
-        return (counter < path.size());
-    }
-
-    public PathNode next() {
-        PathNode res = path.get(counter);
-        counter++;
-        return res;
-    }
-
-    public void add(PathNode a) {
-        path.add(a);
-    }
-
-    /*public void remove(Cord a){
-    PathNode b;
-    for(int i = 0; i < path.size(); i++){
-    b = path.get(i);
-    if(b.getX() == a.getX() && b.getY() == a.getY()){
-    path.remove(b);
-    break;
-    }
-    }
-    if(counter > 0)
-    counter--;
-    }*/
-    public boolean contains(Point a) {
-        boolean res = false;
-        PathNode b;
-        for (int i = 0; i < path.size(); i++) {
-            b = path.get(i);
-            if (b.getX() == a.x && b.getY() == a.y) {
-                res = true;
-                break;
-            }
+        public Path()
+        {
+                path = new ArrayList<PathNode>();
+                counter = 0;
         }
-        return res;
-    }
 
-    public boolean containsLower(PathNode a) {
-        boolean res = false;
-        PathNode b;
-        for (int i = 0; i < path.size(); i++) {
-            b = path.get(i);
-            if (b.getX() == a.getX() && b.getY() == a.getY() && b.getCount() <= a.getCount()) {
-                res = true;
-                break;
-            }
+        public boolean hasNext()
+        {
+                return (counter < path.size());
         }
-        return res;
-    }
 
-    /*public String toString(){
-    String res = "";
-    String a = "\n";
-    for(PathNode b : path){
-    res += b.toString() + a;
-    }
-    return res;
-    }*/
-    public PathNode findNext(PathNode end) {
-        PathNode res = null;
-        PathNode b;
-        for (int i = 0; i < path.size(); i++) {
-            b = path.get(i);
-            if (b.adjacent(end)) {
-                res = b;
-                break;
-            }
+        public PathNode next()
+        {
+                PathNode res = path.get(counter);
+                counter++;
+                return res;
         }
-        return res;
-    }
+
+        public void add(PathNode a)
+        {
+                path.add(a);
+        }
+
+        public boolean contains(Point a)
+        {
+                boolean res = false;
+                PathNode b;
+                for (int i = 0; i < path.size(); i++)
+                {
+                        b = path.get(i);
+                        if (b.getX() == a.x && b.getY() == a.y)
+                        {
+                                res = true;
+                                break;
+                        }
+                }
+                return res;
+        }
+
+        public boolean containsLower(PathNode a)
+        {
+                boolean res = false;
+                PathNode b;
+                for (int i = 0; i < path.size(); i++)
+                {
+                        b = path.get(i);
+                        if (b.getX() == a.getX() && b.getY() == a.getY() && b.getCount() <= a.getCount())
+                        {
+                                res = true;
+                                break;
+                        }
+                }
+                return res;
+        }
+
+        public PathNode findNext(PathNode end)
+        {
+                PathNode res = null;
+                PathNode b;
+                for (int i = 0; i < path.size(); i++)
+                {
+                        b = path.get(i);
+                        if (b.adjacent(end))
+                        {
+                                res = b;
+                                break;
+                        }
+                }
+                return res;
+        }
 }
