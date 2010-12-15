2aa1f8697fb8dd94f0277bf0099f61413c490b6c
maartenvandenhoek <maartenvandenhoek@3c75896a-3ff0-c1a0-4aaa-65231e0fef79>
2010-12-15 12:45:57 +0000

    Fields verwijderd (nu in TowerData.java)

--- trunk/code/TowerDefence/src/Tower.java
+++ trunk/code/TowerDefence/src/Tower.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -6,45 +5,7 @@ import java.awt.Graphics;
 public class Tower extends Field
 {
 
-    private int range;
-    private int hitPoints;
     private int caseNumber;
-    private int cost;
-    private int previousCaseNumber;
-    //Missle stats
-    private int missleSpeed;
-    private int missleDamage;
-    private int missleImage;
-
-    public int getMissleDamage()
-    {
-        return missleDamage;
-    }
-
-    public void setMissleDamage(int missleDamage)
-    {
-        this.missleDamage = missleDamage;
-    }
-
-    public int getMissleImage()
-    {
-        return missleImage;
-    }
-
-    public void setMissleImage(int missleImage)
-    {
-        this.missleImage = missleImage;
-    }
-
-    public int getMissleSpeed()
-    {
-        return missleSpeed;
-    }
-
-    public void setMissleSpeed(int missleSpeed)
-    {
-        this.missleSpeed = missleSpeed;
-    }
 
     public Tower()
     {
@@ -56,69 +17,31 @@ public class Tower extends Field
     {
         super();
         setCaseNumber(tower);
-        switch (tower)
-        {
-            case 1:
-            {
-                //setImage(1);
-                setHitPoints(250);
-                setMissleDamage(10);
-                setMissleImage(0);
-                setMissleSpeed(20);
-                setRange(150);
-                setCost(10);
-                setPreviousCaseNumber(0);
-            }
-            case 11:
-            {
-                //setImage(1);
-                setHitPoints(300);
-                setMissleDamage(20);
-                setMissleImage(0);
-                setMissleSpeed(20);
-                setRange(175);
-                setCost(20);
-                setPreviousCaseNumber(1);
-            }
-        }
+    }
+
+
+    /**
+     * 
+     * @return
+     */
+    public int getCaseNumber()
+    {
+        return caseNumber;
     }
 
     /**
      *
-     * @param rangeIn int
+     * @param caseNumber
      */
-    public void setRange(int rangeIn)
+    public void setCaseNumber(int caseNumber)
     {
-        range = rangeIn;
+        this.caseNumber = caseNumber;
     }
 
     /**
-     *
-     * @param hp int
+     * 
+     * @param g
      */
-    public void setHitPoints(int hp)
-    {
-        hitPoints = hp;
-    }
-
-    /**
-     *
-     * @return range
-     */
-    public int getRange()
-    {
-        return range;
-    }
-
-    /**
-     *
-     * @return hitpoints
-     */
-    public int getHitPoints()
-    {
-        return hitPoints;
-    }
-
     public void paint(Graphics g)
     {
         // Execute the paint function as if it was a Field object
@@ -128,35 +51,5 @@ public class Tower extends Field
         // Add a filled rectangle to the graphics
         g.setColor(new Color(0, 255, 0));
         g.fillRect(size.width / 4, size.height / 4, size.width / 2, size.height / 2);
-    }
-
-    public int getCaseNumber()
-    {
-        return caseNumber;
-    }
-
-    public void setCaseNumber(int caseNumber)
-    {
-        this.caseNumber = caseNumber;
-    }
-
-    public int getCost()
-    {
-        return cost;
-    }
-
-    public void setCost(int cost)
-    {
-        this.cost = cost;
-    }
-
-    public int getPreviousCaseNumber()
-    {
-        return previousCaseNumber;
-    }
-
-    public void setPreviousCaseNumber(int previousCaseNumber)
-    {
-        this.previousCaseNumber = previousCaseNumber;
     }
 }
