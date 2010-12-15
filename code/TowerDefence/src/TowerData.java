f9a0d048a34438a3dae8f38c2ec82a05156f5667
maartenvandenhoek <maartenvandenhoek@3c75896a-3ff0-c1a0-4aaa-65231e0fef79>
2010-12-15 15:06:02 +0000

    Tower caseNumbers veranderd. get functies afgemaakt (van de 8 huidige properties)

--- code/TowerDefence/src/TowerData.java
+++ code/TowerDefence/src/TowerData.java
@@ -15,6 +15,7 @@ public class TowerData {
      * 5    missleDamage
      * 6    missleImage
      * 7    missleSpeed
+     * 8    towerImage
      */
     private int[][] towerData;
 
@@ -23,7 +24,7 @@ public class TowerData {
      */
     public TowerData(){
         towerData = new int[3][8];
-        towerData[0][0] = 1;        //caseNumber
+        towerData[0][0] = 0;        //caseNumber
         towerData[0][1] = 200;      //costs
         towerData[0][2] = 100;      //hitPoints
         towerData[0][3] = 100;      //range
@@ -33,21 +34,21 @@ public class TowerData {
         towerData[0][7] = 10;       //missleSpeed
         towerData[0][8] = 1;        //towerImage
 
-        towerData[1][0] = 2;        //caseNumber
+        towerData[1][0] = 1;        //caseNumber
         towerData[1][1] = 250;      //costs
         towerData[1][2] = 100;      //hitPoints
         towerData[1][3] = 100;      //range
-        towerData[1][4] = 0;        //previousCaseNumber
+        towerData[1][4] = 1;        //previousCaseNumber
         towerData[1][5] = 2;        //missleDamage
         towerData[1][6] = 1;        //missleImage
         towerData[1][7] = 15;       //missleSpeed
         towerData[1][8] = 1;        //towerImage
 
-        towerData[2][0] = 3;        //caseNumber
+        towerData[2][0] = 2;        //caseNumber
         towerData[2][1] = 275;      //costs
         towerData[2][2] = 100;      //hitPoints
         towerData[2][3] = 100;      //range
-        towerData[2][4] = 0;        //previousCaseNumber
+        towerData[2][4] = 1;        //previousCaseNumber
         towerData[2][5] = 2;        //missleDamage
         towerData[2][6] = 2;        //missleImage
         towerData[2][7] = 20;       //missleSpeed
@@ -56,97 +57,100 @@ public class TowerData {
 
     /**
      * Returns the hitpoints of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the costs of a tower
      */
     public int getCaseNumber(int caseNumber){
-        return towerData[caseNumber - 1][0];
+        return towerData[caseNumber][0];
     }
 
     /**
      * Returns the costs of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the costs of a tower
      */
     public int getCosts(int caseNumber){
-        return towerData[caseNumber - 1][1];
+        return towerData[caseNumber][1];
     }
 
     /**
      * Returns the hitpoints of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the hitpoints of a tower
      */
     public int getHitpoints(int caseNumber){
-        return towerData[caseNumber - 1][2];
+        return towerData[caseNumber][2];
     }
 
     /**
      * Returns the range of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the range of a tower
      */
     public int getRange(int caseNumber){
-        return towerData[caseNumber - 1][3];
+        return towerData[caseNumber][3];
     }
 
     /**
      * Returns the previous caseNumber of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the previous casenumber of a tower
      */
     public int getPreviousCaseNumber(int caseNumber){
-        return towerData[caseNumber - 1][4];
+        return towerData[caseNumber][4];
     }
 
     /**
      * Returns the missle damage of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the missle damage of a tower
      */
     public int getMissleDamage(int caseNumber){
-        return towerData[caseNumber - 1][5];
+        return towerData[caseNumber][5];
     }
 
     /**
-     * Returns the imagenumber of the towers missle where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * Returns the image number of the towers missle where the caseNumber of the tower
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
      * @return the image number of a missle
      */
     public int getMissleImage(int caseNumber){
-        return towerData[caseNumber - 1][6];
+        return towerData[caseNumber][6];
     }
 
     /**
      * Returns the missle speed of the tower where the caseNumber of the tower
-     * equals the parameter caseNumber. De caseNumber is used as caseNumber
-     * minus one, because the two dimensial array is zero-based.
+     * equals the parameter caseNumber.
      *
      * @param caseNumber the caseNumber of the towertype
-     * @return the missle speed of a tower
+     * @return the missle speed of a missle
      */
     public int getMissleSpeed(int caseNumber){
-        return towerData[caseNumber - 1][7];
+        return towerData[caseNumber][7];
+    }
+
+    /**
+     * Returns the towerimage number where the caseNumber of the tower
+     * equals the parameter caseNumber.
+     *
+     * @param caseNumber the caseNumber of the towertype
+     * @return the towerimage number
+     */
+    public int getTowerImage(int caseNumber){
+        return towerData[caseNumber][8];
     }
 }
