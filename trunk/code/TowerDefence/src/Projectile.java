0e22f7bd9e7d28596c227331ddac79d9e8fe3538
andra.veraart <andra.veraart@3c75896a-3ff0-c1a0-4aaa-65231e0fef79>
2010-12-16 11:28:29 +0000

    Dit keer Projectile ook toegevoegd aan SVN...

---
+++ trunk/code/TowerDefence/src/Projectile.java
@@ -0,0 +1,29 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author averaart
+ */
+public class Projectile extends Sprite {
+
+    int damage;
+
+    Projectile(int d) {
+        super();
+        damage = d;
+    }
+
+    private void endMove() {
+        System.out.println("BOEM!!!/nIk doe nu " + damage + " schade aan m'n doelwit");
+        try {
+            this.finalize();
+        } catch (Throwable ex) {
+        }
+    }
+}
