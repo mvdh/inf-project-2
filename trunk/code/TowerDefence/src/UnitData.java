200e759dda4cd0c88c4425158aa6de31582fd6cc
maartenvandenhoek <maartenvandenhoek@3c75896a-3ff0-c1a0-4aaa-65231e0fef79>
2011-01-21 09:48:33 +0000

    overbodige dingen gecommentarieerd

--- trunk/code/TowerDefence/src/UnitData.java
+++ trunk/code/TowerDefence/src/UnitData.java
@@ -11,8 +11,8 @@ public class UnitData
 	 * 3 Image
 	 * 4 Speed
 	 */
-	final private int[][] unitData;
-	final private String[] unitImages;	
+//	final private int[][] unitData;
+//	final private String[] unitImages;	
 	private GameStats stats;
 
 
@@ -22,48 +22,48 @@ public class UnitData
 	public UnitData(GameStats statsIn)
 	{
 		stats = statsIn;
-		unitData = new int[8][4];
-		unitImages = new String[7];
-
-		unitData[0][0] = 1; // reward
-		unitData[0][1] = 100; // hitPoints
-		unitData[0][2] = 100; // Damage
-		unitData[0][3] = 2; // Speed
-
-		unitData[1][0] = 1; // reward
-		unitData[1][1] = 200; // hitPoints
-		unitData[1][2] = 100; // Damage
-		unitData[1][3] = 2; // Speed
-
-		unitData[2][0] = 1; // reward
-		unitData[2][1] = 400; // hitPoints
-		unitData[2][2] = 100; // Damage
-		unitData[2][3] = 2; // Speed
-
-		unitData[3][0] = 2; // reward
-		unitData[3][1] = 800; // hitPoints
-		unitData[3][2] = 100; // Damage
-		unitData[3][3] = 2; // Speed
-
-		unitData[4][0] = 2; // reward
-		unitData[4][1] = 1600; // hitPoints
-		unitData[4][2] = 100; // Damage
-		unitData[4][3] = 2; // Speed
-
-		unitData[5][0] = 2; // reward
-		unitData[5][1] = 3200; // hitPoints
-		unitData[5][2] = 100; // Damage
-		unitData[5][3] = 3; // Speed
-
-		unitData[6][0] = 3; // reward
-		unitData[6][1] = 6400; // hitPoints
-		unitData[6][2] = 100; // Damage
-		unitData[6][3] = 2; // Speed
-
-                unitData[7][0] = 3; // reward
-		unitData[7][1] = 12800; // hitPoints
-		unitData[7][2] = 100; // Damage
-		unitData[7][3] = 3; // Speed                unitData[7][]
+//		unitData = new int[8][4];
+//		unitImages = new String[7];
+//
+//		unitData[0][0] = 1; // reward
+//		unitData[0][1] = 100; // hitPoints
+//		unitData[0][2] = 100; // Damage
+//		unitData[0][3] = 2; // Speed
+//
+//		unitData[1][0] = 1; // reward
+//		unitData[1][1] = 200; // hitPoints
+//		unitData[1][2] = 100; // Damage
+//		unitData[1][3] = 2; // Speed
+//
+//		unitData[2][0] = 1; // reward
+//		unitData[2][1] = 400; // hitPoints
+//		unitData[2][2] = 100; // Damage
+//		unitData[2][3] = 2; // Speed
+//
+//		unitData[3][0] = 2; // reward
+//		unitData[3][1] = 800; // hitPoints
+//		unitData[3][2] = 100; // Damage
+//		unitData[3][3] = 2; // Speed
+//
+//		unitData[4][0] = 2; // reward
+//		unitData[4][1] = 1600; // hitPoints
+//		unitData[4][2] = 100; // Damage
+//		unitData[4][3] = 2; // Speed
+//
+//		unitData[5][0] = 2; // reward
+//		unitData[5][1] = 3200; // hitPoints
+//		unitData[5][2] = 100; // Damage
+//		unitData[5][3] = 3; // Speed
+//
+//		unitData[6][0] = 3; // reward
+//		unitData[6][1] = 6400; // hitPoints
+//		unitData[6][2] = 100; // Damage
+//		unitData[6][3] = 2; // Speed
+//
+//                unitData[7][0] = 3; // reward
+//		unitData[7][1] = 12800; // hitPoints
+//		unitData[7][2] = 100; // Damage
+//		unitData[7][3] = 3; // Speed                unitData[7][]
 
 		/*
 		unitImages[0] = "unit-mario1-f";
@@ -75,15 +75,15 @@ public class UnitData
 		unitImages[6] = "D1";
 		 */
 	}
-
-	public int getReward(int caseNumber)
-	{
-		return unitData[caseNumber][0];
-	}
+//
+//	public int getReward(int caseNumber)
+//	{
+//		return unitData[caseNumber][0];
+//	}
 
 	public int getDamage()
 	{
-		return 25;//unitData[caseNumber][2];
+		return 25;
 	}
 
 	public Unit getNewUnit(int waveNumber)
